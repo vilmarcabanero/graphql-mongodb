57fbbcf6e0192d8099f60550b58889ec02caec79
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-08-11 19:25:47 +0800

    Done connecting student to lesson

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,11 +1,24 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
 import { CreateLessonInput } from './lesson.input';
+import { AssignStudentsToLessonInput } from './assign-students-to-lesson.input';
+import { Lesson } from './lesson.entity';
+import { StudentService } from '../student/student.service';
 
 @Resolver(of => LessonType)
 export class LessonResolver {
-  constructor(private lessonService: LessonService) {}
+  constructor(
+    private lessonService: LessonService,
+    private studentService: StudentService,
+  ) {}
 
   @Query(returns => LessonType)
   lesson(@Args('id') id: string) {
@@ -22,5 +35,19 @@ export class LessonResolver {
     @Args('createLessonInput') createLessonInput: CreateLessonInput,
   ) {
     return this.lessonService.createLesson(createLessonInput);
+  }
+
+  @Mutation(returns => LessonType)
+  assignStudentsToLesson(
+    @Args('assignStudentsToLessonInput')
+    assignStudentsToLessonInput: AssignStudentsToLessonInput,
+  ) {
+    const { lessonId, studentIds } = assignStudentsToLessonInput;
+    return this.lessonService.assignStudentsToLesson(lessonId, studentIds);
+  }
+
+  @ResolveField()
+  students(@Parent() lesson: Lesson) {
+    return this.studentService.getManyStudents(lesson.students);
   }
 }
--- src/lesson/lesson.type.ts
+++ src/lesson/lesson.type.ts
@@ -1,8 +1,9 @@
 import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { StudentType } from 'src/student/student.type';
 
 @ObjectType('Lesson')
 export class LessonType {
-  @Field((type) => ID)
+  @Field(type => ID)
   id: string;
 
   @Field()
@@ -13,4 +14,7 @@ export class LessonType {
 
   @Field()
   endDate: string;
+
+  @Field(() => [StudentType])
+  students: string;
 }
